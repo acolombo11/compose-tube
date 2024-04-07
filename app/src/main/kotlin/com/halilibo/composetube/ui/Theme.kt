37874cc1b82b92f498a1827810dd77f63ea64288
Andrea Colombo <acolombodev@gmail.com>
2024-04-07 18:54:28 +0200

    Clean theme

--- app/src/main/kotlin/com/halilibo/composetube/ui/Theme.kt
+++ app/src/main/kotlin/com/halilibo/composetube/ui/Theme.kt
@@ -19,7 +19,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposeTubeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ComposeTubeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
@@ -28,7 +31,6 @@ fun ComposeTubeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compo
 
     MaterialTheme(
         colors = colors,
-        typography = typography,
         shapes = shapes,
         content = content
     )
